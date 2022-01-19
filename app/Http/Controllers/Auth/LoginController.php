94823a4a99725493d4f631448a9011eae17d1833
AxlivanRz <a18050033@itsperote.edu.mx>
2022-01-19 16:23:56 -0600

    nuevo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB; 
 use Illuminate\Support\Facades\HASH;
 
 
--- resources/views/Usuarios/register.blade.php
+++ resources/views/Usuarios/register.blade.php
@@ -11,48 +11,32 @@
                 <div class="card-body">
                     <form action="{{route('usuarios.store')}}" method="POST" ><!--enctype="multipart/form-data", sirve para enviar imagen -->
                         @csrf 
-                            <div class="results">
-                                @if (Session::get('Success'))
-                                    <div class="alert alert-success">
-                                        {{Session::get('success')}}
-                                    </div>                                    
-                                @endif
-                            </div>
-                            <div class="results">
-                                @if (Session::get('fail'))
-                                    <div class="alert alert-danger">
-                                        {{Session::get('fail')}}
-                                    </div>                                    
-                                @endif
-                            </div>
-                        
-
                         <div class="form-group row">
                             <div class="form-group col-md-4">  
-                                <label for="nombre_usuario">Nombre</label>
+                                <label for="nombre">Nombre</label>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text"><i class="far fa-address-book"></i></span>
                                     </div>
-                                    <input id="nombre_usuario" type="text" class="form-control" name="nombre_usuario" value="{{ old('nombre_usuario') }}" required autocomplete="nombre_usuario" autofocus>
+                                    <input id="nombre" type="text" class="form-control" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
                                 </div>
                             </div>
                             <div class="form-goup col-md-4">
-                                <label for="apellido_paterno_usuario">Primer Apellido</label>
+                                <label for="primer_Apellido">Primer Apellido</label>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text"><i class="far fa-address-book"></i></span>
                                     </div>
-                                    <input id="apellido_paterno_usuario" type="text" class="form-control" name="apellido_paterno_usuario" value="{{ old('apellido_paterno_usuario') }}" required autocomplete="apellido_paterno_usuario" autofocus>
+                                    <input id="primer_Apellido" type="text" class="form-control" name="primer_Apellido" value="{{ old('primer_Apellido') }}" required autocomplete="primer_Apellido" autofocus>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
-                                <label for=apellido_materno_usuario" >Segundo Apellido</label>
+                                <label for="segundo_Apellido" >Segundo Apellido</label>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text"><i class="far fa-address-book"></i></span>
                                     </div>
-                                    <input id="apellido_materno_usuario" type="text" class="form-control" name="apellido_materno_usuario" value="{{ old('apellido_materno_usuario') }}" autocomplete="apellido_materno_usuario" autofocus>
+                                    <input id="segundo_Apellido" type="text" class="form-control" name="segundo_Apellido" value="{{ old('segundo_Apellido') }}" autocomplete="segundo_Apellido" autofocus>
                                 </div>
                             </div>
                         </div>
@@ -112,14 +96,13 @@
                                     <input id="correo_electronico" type="email" class="form-control" name="correo_electronico" value="{{ old('correo_electronico') }}" required autocomplete="correo_electronico">
                                 </div>
                             </div>
-
                             <div class="form-group col-md-4">
-                                <label for="contrasena_usuario">Contraseña</label>
+                                <label for="contrasena">Contraseña</label>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend ">
                                     <span class="input-group-text "><i class="fas fa-lock"></i></span>
                                     </div>
-                                    <input id="contrasena_usuario" type="password" class="form-control" name="contrasena_usuario" required autocomplete="contrasena_usuario">
+                                    <input id="contrasena" type="password" class="form-control" name="contrasena" required autocomplete="contrasena">
                                 </div>   
                             </div>
                         </div>
